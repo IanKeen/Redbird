341a59b2ce9e251c54a0bdad76905c0c6875de4b
Ian Keen <iankeen82@gmail.com>
2016-08-24 22:28:23 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "Redbird",
 	targets: [
-		Target(name: "Redbird"),
-		Target(name: "RedbirdExample", dependencies: ["Redbird"])
+		Target(name: "Redbird")
 	],
 	dependencies: [
 		.Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 12)
